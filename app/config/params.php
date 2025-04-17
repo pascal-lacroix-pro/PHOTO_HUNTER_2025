70e8e5ea222035bffeb31e4f20eeecb33a6e3dbe
Pascal Lacroix <p.lacroix@promotion-sociale.be>
2025-04-17 15:36:28 +0200

    Structure MVC de base

---
+++ app/config/params.php
@@ -0,0 +1,10 @@
+<?php
+
+// Initialisation des zones dynamiques
+$content = "";
+
+// Paramètres de connexion
+define('DB_HOST', '127.0.0.1:8889');
+define('DB_NAME', 'photo_hunter');
+define('DB_USER', 'root');
+define('DB_PASSWORD', 'root');
