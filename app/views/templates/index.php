113ebeb8fd2ed6138952f471fd01ab3eb8d6c5ff
Pascal Lacroix <p.lacroix@promotion-sociale.be>
2025-04-24 13:36:26 +0200

    Intégration et découpage du template fourni

--- app/views/templates/index.php
+++ app/views/templates/index.php
@@ -1,20 +1,16 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <?php include '../app/views/templates/partials/_head.php'; ?>
 </head>
 
-<body>
-    <header>HEADER</header>
-    <main>
-        MAIN
-        <?php echo $content; ?>
-        <?php var_dump($connexion); ?>
-    </main>
-    <footer>FOOTER</footer>
+<body class="bg-gray-300">
+    <?php include '../app/views/templates/partials/_nav.php'; ?>
+
+    <?php include '../app/views/templates/partials/_main.php'; ?>
+
+    <?php include '../app/views/templates/partials/_footer.php'; ?>
 </body>
 
 </html>
---
+++ app/views/templates/partials/_main.php
@@ -0,0 +1,12 @@
+<main class="my-10">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="flex flex-wrap -m-4">
+            <div class="p-4 md:w-2/3">
+                <?php echo $content; ?>
+            </div>
+            <!-- ASIDE -->
+
+            <?php include '../app/views/templates/partials/_aside.php'; ?>
+        </div>
+    </div>
+</main>
